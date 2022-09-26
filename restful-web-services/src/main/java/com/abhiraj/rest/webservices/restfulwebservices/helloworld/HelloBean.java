8cdeb7e70b3f56aa20d40f91bd2af16111e1977f
Abhiraj Suryawanshi <suryawanshi.abhiraj1@gmail.com>
2022-09-26 18:30:11 -0300

    Added Hello World Bean

---
+++ restful-web-services/src/main/java/com/abhiraj/rest/webservices/restfulwebservices/helloworld/HelloBean.java
@@ -0,0 +1,25 @@
+package com.abhiraj.rest.webservices.restfulwebservices.helloworld;
+
+public class HelloBean {
+
+    private String message;
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public HelloBean(String message) {
+        this.message = message;
+    }
+
+    @Override
+    public String toString() {
+        return "HelloBean{" +
+                "message='" + message + '\'' +
+                '}';
+    }
+}
--- restful-web-services/src/main/java/com/abhiraj/rest/webservices/restfulwebservices/helloworld/HelloWorldController.java
+++ restful-web-services/src/main/java/com/abhiraj/rest/webservices/restfulwebservices/helloworld/HelloWorldController.java
@@ -4,10 +4,15 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class HelloWorld {
+public class HelloWorldController {
 
     @GetMapping(path = "/helloWorld")
     public String helloWorld(){
         return "Hello World";
+    }
+
+    @GetMapping(path = "/helloBean")
+    public HelloBean helloBean(){
+        return new HelloBean("Hello World!");
     }
 }
