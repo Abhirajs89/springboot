a00af9103d3607924a3f88415a873ef28b135dee
Anuja <suryawanshi.abhiraj1@gmail.com>
2022-09-26 18:13:14 -0300

    Added Hello World Controller

---
+++ restful-web-services/src/main/java/com/abhiraj/rest/webservices/restfulwebservices/helloworld/HelloWorld.java
@@ -0,0 +1,13 @@
+package com.abhiraj.rest.webservices.restfulwebservices.helloworld;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorld {
+
+    @GetMapping(path = "/helloWorld")
+    public String helloWorld(){
+        return "Hello World";
+    }
+}
