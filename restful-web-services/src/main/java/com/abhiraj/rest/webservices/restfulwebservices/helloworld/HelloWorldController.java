05f3f8293fb203a169386fa4a45b5d4e49bf0f3c
Abhiraj Suryawanshi <suryawanshi.abhiraj1@gmail.com>
2022-09-26 19:05:48 -0300

    Added @PathVariable for @GetMapping(path = "/helloBean/pathVariable/{name}")

--- restful-web-services/src/main/java/com/abhiraj/rest/webservices/restfulwebservices/helloworld/HelloWorldController.java
+++ restful-web-services/src/main/java/com/abhiraj/rest/webservices/restfulwebservices/helloworld/HelloWorldController.java
@@ -1,6 +1,7 @@
 package com.abhiraj.rest.webservices.restfulwebservices.helloworld;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -14,5 +15,10 @@ public class HelloWorldController {
     @GetMapping(path = "/helloBean")
     public HelloBean helloBean(){
         return new HelloBean("Hello World!");
+    }
+
+    @GetMapping(path = "/helloBean/pathVariable/{name}")
+    public HelloBean helloBeanPathVariable(@PathVariable String name){
+        return new HelloBean("Hello World "+name+"!");
     }
 }
